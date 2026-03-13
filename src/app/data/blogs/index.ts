6805013dd8e63134e6e1138a1ab689533ff8466f
NIXONHS <nixon2000paul@gmail.com>
2026-03-12 23:27:45 -0500

    Feat: Agregar blog Introduccion, Integracion y Repositorio de Herramientas CASE (Clase)

--- src/app/data/blogs/index.ts
+++ src/app/data/blogs/index.ts
@@ -9,5 +9,14 @@ import { blog3 } from './static-3';
 import { blog4 } from './static-4';
 import { blogTaxonomiaCASE } from './taxonomia-case';
 import { blogICaseEvolution } from './i-case-tools';
+import { blogCaseFundamentos } from './case-fundamentos';
 
-export const STATIC_BLOGS: Blog[] = [blog1, blog2, blog3, blog4, blogTaxonomiaCASE, blogICaseEvolution];
+export const STATIC_BLOGS: Blog[] = [
+	blog1,
+	blog2,
+	blog3,
+	blog4,
+	blogTaxonomiaCASE,
+	blogICaseEvolution,
+	blogCaseFundamentos,
+];
